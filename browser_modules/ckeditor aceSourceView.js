578f53bc62fa968a49bbe2621f0e3afa5e8a84b0
Matthew Kastor <matthewkastor@gmail.com>
2013-03-25 04:18:31 -0400

    add lint options

--- browser_modules/ckeditor aceSourceView.js
+++ browser_modules/ckeditor aceSourceView.js
@@ -1,6 +1,17 @@
+/*jslint
+    indent: 4,
+    maxerr: 50,
+    white: true,
+    browser: true,
+    vars: true
+*/
+/*globals
+    module
+*/
 module.exports = {
     'url' : 'ace editor.html?mode=ace/mode/html',
     'hook' : function (codeHighlighter) {
+        "use strict";
         codeHighlighter.getValue = function () {
             return codeHighlighter.frame.contentWindow.ui.session.getValue();
         };
@@ -9,6 +20,6 @@ module.exports = {
         };
         return codeHighlighter;
     }
-}
+};
 
 
