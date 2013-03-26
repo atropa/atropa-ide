eeb2351f420c388af48279697234693192c8de78
Matthew Kastor <matthewkastor@gmail.com>
2013-03-25 23:14:08 -0400

    added jake and atropa-jsformatter

---
+++ node_modules/atropa-jsformatter/src/atropa-jsformatter.js
@@ -0,0 +1,72 @@
+"use strict";
+/*jslint
+    white: true,
+    node: true
+*/
+
+/**
+ * Pretty prints javaScript using esprima and escodegen. Line endings
+ *  will be converted to the value stored in os.EOL
+ * @param {String} source The source code to prettify.
+ * @param {Object} escodegenOptions Optional. Use different settings for
+ *  escodegen.
+ * @param {Object} esprimaOptions Optional. Use different settings for
+ *  esprima.
+ * @see <a href="http://esprima.org/doc/">http://esprima.org/doc/</a>
+ * @see <a href="https://github.com/Constellation/escodegen/wiki/API">
+ *  https://github.com/Constellation/escodegen/wiki/API</a>
+ * @requires esprima
+ * @requires escodegen
+ * @requires os
+ * @example
+ *  var formatter = require('atropa-jsformatter');
+ *  var out = formatter('function wobble() { return "flam"; }');
+ *  console.log(out);
+ */
+function formatJs(source, escodegenOptions, esprimaOptions) {
+    
+    var esprima, escodegen, os, tree;
+    
+    esprima = require('esprima');
+    escodegen = require('escodegen');
+    os = require('os');
+    
+    esprimaOptions = esprimaOptions || {
+        "loc"      : false,
+        "range"    : true,
+        "raw"      : false,
+        "tokens"   : true,
+        "comment"  : true,
+        "tolerant" : false
+    };
+    
+    escodegenOptions = escodegenOptions || {
+        "format" : {
+            "indent" : {
+                "style" : "    ",
+                "base"  : 0,
+                "adjustMultilineComment" : true
+            },
+            "json"       : false,
+            "renumber"   : false,
+            "hexadecimal": false,
+            "quotes"     : "single",
+            "escapeless" : false,
+            "compact"    : false,
+            "parentheses": true,
+            "semicolons" : true
+        },
+        "parse"    : null,
+        "comment"  : true,
+        "sourceMap": undefined
+    };
+    
+    tree = esprima.parse(source, esprimaOptions);
+    tree = escodegen.attachComments(tree, tree.comments, tree.tokens);
+    
+    return escodegen.generate(
+        tree, escodegenOptions).replace(/(\r\n|\r|\n)/g, os.EOL);
+};
+
+
+module.exports = formatJs;
