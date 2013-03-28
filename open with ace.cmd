c603641800cce18c73db60d978e347a5eaaf0f7d
Matthew Kastor <matthewkastor@gmail.com>
2013-03-27 22:37:41 -0400

    added file to load files into ace after server is started.

---
+++ open with ace.cmd
@@ -0,0 +1,6 @@
+REM The atropa server must be started before using this.
+REM also the port number will need to be changed if
+REM you're running the server on a different port
+
+start "loads file in your browser" "http://localhost:9999/ace editor.html?loadFile=%~1"
+
